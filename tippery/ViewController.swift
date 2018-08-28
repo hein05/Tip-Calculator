2edc48d71960c50f1691e3c1f016c6b10b2748ea
hein05 <heinsoe05@outlook.com>
2018-08-28 18:54:11 -0400

    Basic functionality and Design

--- tippery/ViewController.swift
+++ tippery/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var billLabel: UITextField!
+   @IBOutlet weak var tipAmt: UILabel!
+   @IBOutlet weak var totalAmt: UILabel!
+   @IBOutlet weak var presetPercent: UISegmentedControl!
+   
+   @IBAction func calTip(_ sender: Any) {
+      let tipPercent = [0.1, 0.15, 0.2]
+      let billAmt = Double(billLabel.text!) ?? 0
+      
+      let tip = billAmt * tipPercent[presetPercent.selectedSegmentIndex]
+      let total = billAmt + tip
+      tipAmt.text = String(format: "$ %.2f", tip)
+      totalAmt.text = String(format: "$ %.2f", total)
+   }
+   
+   @IBAction func onTap(_ sender: Any) {
+      view.endEditing(true)
+   }
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
